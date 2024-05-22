359cc6bbe18a0bd7e5ee930751d138269dc16685
Miguel Castilho <miguelcastilhodias@gmail.com>
2024-05-22 12:48:05 +0200

    delete files

--- upload_prod_env_file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-base64 -i inventory/prod.yml -o inventory/encoded_env_file
-gh secret delete ENCODED_ENV_FILE
-gh secret set ENCODED_ENV_FILE --body "$(cat inventory/encoded_env_file)"
-rm inventory/encoded_env_file
